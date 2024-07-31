5e5ad11af06a95984b3175e85bbd4ab7d7bc8e11
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-07-31 23:38:56 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,15 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
+});
+
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
+});
